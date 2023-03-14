c78d804d35ded981549b5254c115d6609a7a006d
kirtanakrishnan <kirtana.k8@gmail.com>
2023-03-14 18:24:48 -0400

    Committing InClass06

--- app/src/main/java/com/example/inclass_krishnan9285/InClass06/InClass06.java
+++ app/src/main/java/com/example/inclass_krishnan9285/InClass06/InClass06.java
@@ -27,6 +27,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 
+/*
+Kirtana Krishnan
+Assignment #6
+ */
 public class InClass06 extends AppCompatActivity {
 
     private OkHttpClient client;
